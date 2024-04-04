0754a53e3287b9074d138290f1c61ddf35a12df0
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-04-04 17:24:38 +0530

    created the js file and done with index and intro js files

---
+++ index.js
@@ -0,0 +1,44 @@
+//fetch JSON data
+fetch("index.json")
+    .then((response) => response.json())
+    .then((data) => {
+        //local storage
+        // document.getElementById("maini").innerHTML= data.main.Intro;
+        localStorage.setItem("homeData", JSON.stringify(data));
+    })
+    .catch((error) => {
+        console.error("Error", error);
+    });
+
+const data = JSON.parse(localStorage.getItem("homeData"));
+
+
+document.getElementById("int").innerHTML = data[0].int;
+document.getElementById("intro").innerHTML = data[0].intro;
+
+document.getElementById("box01Subhead01").innerHTML = data[1].box01.subhead01;
+document.getElementById("box01Para01").innerHTML = data[1].box01.para01;
+document.getElementById("box01Para02").innerHTML = data[1].box01.para02;
+
+document.getElementById("box02Subhead01").innerHTML = data[2].box02.subhead01;
+document.getElementById("box02Para01").innerHTML = data[2].box02.para01;
+document.getElementById("box02Para02").innerHTML = data[2].box02.para02;
+
+document.getElementById("box03Subhead01").innerHTML = data[3].box03.subhead01;
+document.getElementById("box03Para01").innerHTML = data[3].box03.para01;
+document.getElementById("box03Para02").innerHTML = data[3].box03.para02;
+
+document.getElementById("listTopic").innerHTML = data[4].list.listhead01;
+
+document.getElementById("listSubTopic01").innerHTML = data[4].list.listSubTopic01;
+document.getElementById("listContent01").innerHTML = data[4].list.thing1;
+document.getElementById("listContent02").innerHTML = data[4].list.thing2;
+document.getElementById("listContent03").innerHTML = data[4].list.thing3;
+document.getElementById("listContent04").innerHTML = data[4].list.thing4;
+
+document.getElementById("listSubTopic02").innerHTML = data[4].list.listSubTopic02;
+document.getElementById("listContent05").innerHTML = data[4].list.thing5;
+document.getElementById("listContent06").innerHTML = data[4].list.thing6;
+document.getElementById("listContent07").innerHTML = data[4].list.thing7;
+document.getElementById("listContent08").innerHTML = data[4].list.thing8;
+document.getElementById("listContent09").innerHTML = data[4].list.thing9;
---
+++ intro.js
@@ -0,0 +1,32 @@
+//fetch JSON data
+fetch("intro.json")
+    .then((response) => response.json())
+    .then((data) => {
+        //local storage
+        // document.getElementById("maini").innerHTML= data.main.Intro;
+        localStorage.setItem("introData", JSON.stringify(data));
+    })
+    .catch((error) => {
+        console.error("Error", error);
+    });
+
+const data = JSON.parse(localStorage.getItem("introData"));
+
+document.getElementById("int").innerHTML = data[0].int;
+
+document.getElementById("box01Subhead01").innerHTML = data[1].box01.subhead01;
+document.getElementById("box01para").innerHTML = data[1].box01.para;
+
+document.getElementById("box02Subhead01").innerHTML = data[2].box02.subhead01;
+document.getElementById("box02para").innerHTML = data[2].box02.para;
+
+document.getElementById("box03Subhead01").innerHTML = data[3].box03.subhead01;
+document.getElementById("box03para").innerHTML = data[3].box03.para;
+
+document.getElementById("box04Subhead01").innerHTML = data[4].box04.subhead01;
+document.getElementById("box04para").innerHTML = data[4].box04.para;
+
+document.getElementById("box05Subhead01").innerHTML = data[5].box05.subhead01;
+document.getElementById("box05para").innerHTML = data[5].box05.para;
+
+// table eka danna one yako

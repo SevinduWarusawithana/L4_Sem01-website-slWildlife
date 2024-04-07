cf35b9ce0dcdf20b8de0f072345fd77fc1167ece
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-04-07 22:27:40 +0530

    news

--- dashboard.js
+++ dashboard.js
@@ -14,3 +14,28 @@ function openPopup() {
 
 
 // let username = document.getElementById('openPopup').textContent = `${username}`;
+
+
+// if (currentUser == "user") {
+//     document.getElementById("newsletterEmails").style.display = "none";
+//   } else {
+//     function sub(json_path) {
+//       // Function to create the body of the table
+//       let code = ``;
+//       for (let i = 0; i < json_path.length; i++) {
+//         code += `<li>${json_path[i]}</li>`;
+//       }
+//       return code;
+//     }
+//     // Assuming subscriptions is retrieved from local storage and is an array of email addresses
+//     const newsletterEmails = JSON.parse(localStorage.getItem("newsletterEmails"));
+
+//     // Check if subscriptions is not null and is an array
+//     if (newsletterEmails && Array.isArray(newsletterEmails)) {
+//       document.getElementById("emialLists").innerHTML =
+//         sub(newsletterEmails);
+//     } else {
+//       // Handle the case where newsletterEmails is not found or not an array
+//       console.error("newsletterEmails data is missing or invalid.");
+//     }
+//   }
